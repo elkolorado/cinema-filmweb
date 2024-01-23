fe4cc3207ab528aac0fe33e5d6a74f12744a6d1e
Filip Zieliński <filip.zielinski@microsoft.wsei.edu.pl>
2024-01-23 15:46:08 +0100

    [feat] add imax tag

--- app/booking.js
+++ app/booking.js
@@ -1,38 +1,34 @@
 import Link from "next/link"
 
-function bookingLangDetails(booking){
-    if(booking.attributeIds.filter(attribute => attribute?.includes("dubbed"))[0]){
+function bookingLangDetails(booking) {
+    if (booking.attributeIds.filter(attribute => attribute?.includes("dubbed"))[0]) {
         return "DUB " + booking.attributeIds.filter(attribute => attribute?.includes("dubbed-lang"))[0]?.split("-")[2]?.toUpperCase()
     }
 
-    if(booking.attributeIds.filter(attribute => attribute?.includes("original-lang-pl"))[0]){
+    if (booking.attributeIds.filter(attribute => attribute?.includes("original-lang-pl"))[0]) {
         return "PL"
     }
-    if(booking.attributeIds.filter(attribute => attribute?.includes("original-lang"))[0]){
+    if (booking.attributeIds.filter(attribute => attribute?.includes("original-lang"))[0]) {
         return booking.attributeIds.filter(attribute => attribute?.includes("original-lang"))[0]?.split("-")[2]?.toUpperCase() + " (NAP " + booking.attributeIds.filter(attribute => attribute?.includes("first-subbed"))[0]?.split("-")[3]?.toUpperCase() + ")"
     }
 
 }
 
-function bookingMovieType(booking){
-    if(booking.attributeIds.filter(attribute => attribute?.includes("3d"))[0]){
-        if(booking.attributeIds.filter(attribute => attribute?.includes("imax"))[0]){
-            return "3D IMAX"
-        }
-        return "3D"
+function bookingMovieType(booking) {
+    let movieType = "";
+    if (booking.attributeIds.filter(attribute => attribute?.includes("3d"))[0]) {
+        movieType += "3D"
     }
 
-    if(booking.attributeIds.filter(attribute => attribute?.includes("2d"))[0]){
-        return
+    if (booking.attributeIds.filter(attribute => attribute?.includes("4dx"))[0]) {
+        movieType += "4DX"
     }
 
-    if(booking.attributeIds.filter(attribute => attribute?.includes("4dx"))[0]){
-        return "4DX"
+    if (booking.attributeIds.filter(attribute => attribute?.includes("imax"))[0]) {
+        movieType += "IMAX"
     }
 
-    if(booking.attributeIds.filter(attribute => attribute?.includes("imax"))[0]){
-        return "IMAX"
-    }
+    return movieType;
 
 }
 
